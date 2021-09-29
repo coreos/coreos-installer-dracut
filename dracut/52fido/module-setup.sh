05ddc0cb0e0517b1ed4a89f1adc0fda52717e95a
Antonio Murdaca <runcom@linux.com>
2021-09-29 14:59:23 +0200

    add a fido dummy service to test order
    Signed-off-by: Antonio Murdaca <runcom@linux.com>

---
+++ dracut/52fido/module-setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+install_and_enable_unit() {
+    unit="$1"; shift
+    target="$1"; shift
+    inst_simple "$moddir/$unit" "$systemdsystemunitdir/$unit"
+    # note we `|| exit 1` here so we error out if e.g. the units are missing
+    # see https://github.com/coreos/fedora-coreos-config/issues/799
+    systemctl -q --root="$initdir" add-requires "$target" "$unit" || exit 1
+}
+
+install() {
+    inst_multiple gpg
+    inst_multiple gpg-agent
+    inst_multiple gpg-connect-agent
+    inst_multiple coreos-installer
+
+    inst_multiple fdo-manufacturing-client
+
+    inst_multiple \
+    realpath \
+    basename   \
+    blkid      \
+    cat        \
+    dirname    \
+    findmnt    \
+    growpart   \
+    realpath   \
+    resize2fs  \
+    tail       \
+    tune2fs    \
+    touch      \
+    xfs_admin  \
+    xfs_growfs \
+    wc         \
+    lsblk      \
+    wipefs
+
+    inst_multiple \
+        awk       \
+        cat       \
+        dd        \
+        grep      \
+        mktemp    \
+        partx     \
+        rm        \
+        sed       \
+        sfdisk    \
+        sgdisk    \
+        find
+        
+    inst_multiple -o \
+        clevis-encrypt-sss \
+        clevis-encrypt-tang \
+        clevis-encrypt-tpm2 \
+        clevis-luks-bind \
+        clevis-luks-common-functions \
+        clevis-luks-unlock \
+        pwmake \
+        tpm2_create
+
+    inst_script "$moddir/mfgclient-service" \
+        "/usr/libexec/mfgclient-service"
+
+    install_and_enable_unit "mfgclient.service" \
+        "default.target"
+}
